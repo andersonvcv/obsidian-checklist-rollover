e11cea0092bbad19607297c8241832801dcec23a
Anderson Valério <andersonvcv@gmail.com>
2024-01-16 20:42:46 -0300

    Add .prettierrc, update package.json, update .editorconfig, update manifest.json, update tsconfig.json, update .eslintrc, add get-todos.ts, add UndoModal.ts

--- main.ts
+++ main.ts
@@ -1,134 +1,321 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Notice, Plugin } from 'obsidian';
+import { getDailyNoteSettings, getAllDailyNotes, getDailyNote } from 'obsidian-daily-notes-interface';
+import UndoModal from './UndoModal';
+import RolloverSettingTab from './RollOverSettingTab';
+import { getTodos } from './get-todos';
 
-// Remember to rename these classes and interfaces!
+const MAX_TIME_SINCE_CREATION = 5000; // 5 seconds
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
+export default class RolloverTodosPlugin extends Plugin {
 	async loadSettings() {
+		const DEFAULT_SETTINGS = {
+			templateHeading: 'none',
+			deleteOnComplete: false,
+			removeEmptyTodos: false,
+			rolloverChildren: false,
+			rolloverOnFileCreate: true
+		};
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	isDailyNotesEnabled() {
+		const dailyNotesPlugin = this.app.internalPlugins.plugins['daily-notes'];
+		const dailyNotesEnabled = dailyNotesPlugin && dailyNotesPlugin.enabled;
+
+		const periodicNotesPlugin = this.app.plugins.getPlugin('periodic-notes');
+		const periodicNotesEnabled = periodicNotesPlugin && periodicNotesPlugin.settings?.daily?.enabled;
+
+		return dailyNotesEnabled || periodicNotesEnabled;
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	getLastDailyNote() {
+		const { moment } = window;
+		let { folder, format } = getDailyNoteSettings();
+
+		folder = this.getCleanFolder(folder);
+		folder = folder.length === 0 ? folder : folder + '/';
+
+		const dailyNoteRegexMatch = new RegExp('^' + folder + '(.*).md$');
+		const todayMoment = moment();
+
+		// get all notes in directory that aren't null
+		const dailyNoteFiles = this.app.vault
+			.getMarkdownFiles()
+			.filter(file => file.path.startsWith(folder))
+			.filter(file => moment(file.path.replace(dailyNoteRegexMatch, '$1'), format, true).isValid())
+			.filter(file => file.basename)
+			.filter(file => this.getFileMoment(file, folder, format).isSameOrBefore(todayMoment, 'day'));
+
+		// sort by date
+		const sorted = dailyNoteFiles.sort(
+			(a, b) => this.getFileMoment(b, folder, format).valueOf() - this.getFileMoment(a, folder, format).valueOf()
+		);
+		return sorted[1];
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	getFileMoment(file, folder, format) {
+		let path = file.path;
+
+		if (path.startsWith(folder)) {
+			// Remove length of folder from start of path
+			path = path.substring(folder.length);
+		}
+
+		if (path.endsWith(`.${file.extension}`)) {
+			// Remove length of file extension from end of path
+			path = path.substring(0, path.length - file.extension.length - 1);
+		}
+
+		return moment(path, format);
 	}
-}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	async getAllUnfinishedTodos(file) {
+		const dn = await this.app.vault.read(file);
+		const dnLines = dn.split(/\r?\n|\r|\n/g);
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+		return getTodos({
+			lines: dnLines,
+			withChildren: this.settings.rolloverChildren
+		});
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	async sortHeadersIntoHierarchy(file) {
+		///console.log('testing')
+		const templateContents = await this.app.vault.read(file);
+		const allHeadings = Array.from(templateContents.matchAll(/#{1,} .*/g)).map(([heading]) => heading);
 
-		containerEl.empty();
+		if (allHeadings.length > 0) {
+			console.log(createRepresentationFromHeadings(allHeadings));
+		}
+	}
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	getCleanFolder(folder) {
+		// Check if user defined folder with root `/` e.g. `/dailies`
+		if (folder.startsWith('/')) {
+			folder = folder.substring(1);
+		}
+
+		// Check if user defined folder with trailing `/` e.g. `dailies/`
+		if (folder.endsWith('/')) {
+			folder = folder.substring(0, folder.length - 1);
+		}
+
+		return folder;
+	}
+
+	async rollover(file = undefined) {
+		console.log('rolling over');
+		/*** First we check if the file created is actually a valid daily note ***/
+		let { folder, format } = getDailyNoteSettings();
+		let ignoreCreationTime = false;
+
+		// Rollover can be called, but we need to get the daily file
+		if (file == undefined) {
+			const allDailyNotes = getAllDailyNotes();
+			file = getDailyNote(window.moment(), allDailyNotes);
+			ignoreCreationTime = true;
+		}
+		if (!file) return;
+
+		folder = this.getCleanFolder(folder);
+
+		// is a daily note
+		if (!file.path.startsWith(folder)) return;
+
+		// is today's daily note
+		const today = new Date();
+		const todayFormatted = window.moment(today).format(format);
+		const filePathConstructed = `${folder}${folder == '' ? '' : '/'}${todayFormatted}.${file.extension}`;
+		if (filePathConstructed !== file.path) return;
+
+		// was just created
+		if (today.getTime() - file.stat.ctime > MAX_TIME_SINCE_CREATION && !ignoreCreationTime) return;
+
+		/*** Next, if it is a valid daily note, but we don't have daily notes enabled, we must alert the user ***/
+		if (!this.isDailyNotesEnabled()) {
+			new Notice(
+				'RolloverTodosPlugin unable to rollover unfinished todos: Please enable Daily Notes, or Periodic Notes (with daily notes enabled).',
+				10000
+			);
+		} else {
+			const { templateHeading, deleteOnComplete, removeEmptyTodos } = this.settings;
+
+			// check if there is a daily note from yesterday
+			const lastDailyNote = this.getLastDailyNote();
+			if (!lastDailyNote) return;
+
+			// TODO: Rollover to subheadings (optional)
+			//this.sortHeadersIntoHierarchy(lastDailyNote)
+
+			// get unfinished todos from yesterday, if exist
+			let todos_yesterday = await this.getAllUnfinishedTodos(lastDailyNote);
+
+			console.log(`rollover-daily-todos: ${todos_yesterday.length} todos found in ${lastDailyNote.basename}.md`);
+
+			if (todos_yesterday.length == 0) {
+				return;
+			}
+
+			// setup undo history
+			let undoHistoryInstance = {
+				previousDay: {
+					file: undefined,
+					oldContent: ''
+				},
+				today: {
+					file: undefined,
+					oldContent: ''
+				}
+			};
+
+			// Potentially filter todos from yesterday for today
+			let todosAdded = 0;
+			let emptiesToNotAddToTomorrow = 0;
+			let todos_today = !removeEmptyTodos ? todos_yesterday : [];
+			if (removeEmptyTodos) {
+				todos_yesterday.forEach((line, i) => {
+					const trimmedLine = (line || '').trim();
+					if (trimmedLine != '- [ ]' && trimmedLine != '- [  ]') {
+						todos_today.push(line);
+						todosAdded++;
+					} else {
+						emptiesToNotAddToTomorrow++;
+					}
+				});
+			} else {
+				todosAdded = todos_yesterday.length;
+			}
+
+			// get today's content and modify it
+			let templateHeadingNotFoundMessage = '';
+			const templateHeadingSelected = templateHeading !== 'none';
+
+			if (todos_today.length > 0) {
+				let dailyNoteContent = await this.app.vault.read(file);
+				undoHistoryInstance.today = {
+					file: file,
+					oldContent: `${dailyNoteContent}`
+				};
+				const todos_todayString = `\n${todos_today.join('\n')}`;
+
+				// If template heading is selected, try to rollover to template heading
+				if (templateHeadingSelected) {
+					const contentAddedToHeading = dailyNoteContent.replace(
+						templateHeading,
+						`${templateHeading}${todos_todayString}`
+					);
+					if (contentAddedToHeading == dailyNoteContent) {
+						templateHeadingNotFoundMessage = `Rollover couldn't find '${templateHeading}' in today's daily not. Rolling todos to end of file.`;
+					} else {
+						dailyNoteContent = contentAddedToHeading;
+					}
+				}
+
+				// Rollover to bottom of file if no heading found in file, or no heading selected
+				if (!templateHeadingSelected || templateHeadingNotFoundMessage.length > 0) {
+					dailyNoteContent += todos_todayString;
+				}
+
+				await this.app.vault.modify(file, dailyNoteContent);
+			}
+
+			// if deleteOnComplete, get yesterday's content and modify it
+			if (deleteOnComplete) {
+				let lastDailyNoteContent = await this.app.vault.read(lastDailyNote);
+				undoHistoryInstance.previousDay = {
+					file: lastDailyNote,
+					oldContent: `${lastDailyNoteContent}`
+				};
+				let lines = lastDailyNoteContent.split('\n');
+
+				for (let i = lines.length; i >= 0; i--) {
+					if (todos_yesterday.includes(lines[i])) {
+						lines.splice(i, 1);
+					}
+				}
+
+				const modifiedContent = lines.join('\n');
+				await this.app.vault.modify(lastDailyNote, modifiedContent);
+			}
+
+			// Let user know rollover has been successful with X todos
+			const todosAddedString = todosAdded == 0 ? '' : `- ${todosAdded} todo${todosAdded > 1 ? 's' : ''} rolled over.`;
+			const emptiesToNotAddToTomorrowString =
+				emptiesToNotAddToTomorrow == 0
+					? ''
+					: deleteOnComplete
+					? `- ${emptiesToNotAddToTomorrow} empty todo${emptiesToNotAddToTomorrow > 1 ? 's' : ''} removed.`
+					: '';
+			const part1 = templateHeadingNotFoundMessage.length > 0 ? `${templateHeadingNotFoundMessage}` : '';
+			const part2 = `${todosAddedString}${todosAddedString.length > 0 ? ' ' : ''}`;
+			const part3 = `${emptiesToNotAddToTomorrowString}${emptiesToNotAddToTomorrowString.length > 0 ? ' ' : ''}`;
+
+			let allParts = [part1, part2, part3];
+			let nonBlankLines = [];
+			allParts.forEach(part => {
+				if (part.length > 0) {
+					nonBlankLines.push(part);
+				}
+			});
+
+			const message = nonBlankLines.join('\n');
+			if (message.length > 0) {
+				new Notice(message, 4000 + message.length * 3);
+			}
+			this.undoHistoryTime = new Date();
+			this.undoHistory = [undoHistoryInstance];
+		}
+	}
+
+	async onload() {
+		console.log('OnLoad');
+		await this.loadSettings();
+		this.undoHistory = [];
+		this.undoHistoryTime = new Date();
+		console.log('AddSettingTab');
+		this.addSettingTab(new RolloverSettingTab(this.app, this));
+
+		this.registerEvent(
+			this.app.vault.on('create', async file => {
+				// Check if automatic daily note creation is enabled
+				console.log(`setting create: ${this.settings.rolloverOnFileCreate}`);
+				if (!this.settings.rolloverOnFileCreate) return;
+				this.rollover(file);
+			})
+		);
+
+		this.addCommand({
+			id: 'obsidian-rollover-daily-todos-rollover',
+			name: 'Rollover Todos Now',
+			callback: () => {
+				this.rollover();
+			}
+		});
+
+		this.addCommand({
+			id: 'obsidian-rollover-daily-todos-undo',
+			name: 'Undo last rollover',
+			checkCallback: checking => {
+				// no history, don't allow undo
+				if (this.undoHistory.length > 0) {
+					const now = window.moment();
+					const lastUse = window.moment(this.undoHistoryTime);
+					const diff = now.diff(lastUse, 'seconds');
+					// 2+ mins since use: don't allow undo
+					if (diff > 2 * 60) {
+						return false;
+					}
+					if (!checking) {
+						new UndoModal(this).open();
+					}
+					return true;
+				}
+				return false;
+			}
+		});
 	}
 }
